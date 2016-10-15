623052b82744d08ac8f7b843468bf49c07fba212
Nishanth Prakash <Nishanth Prakash>
2016-10-15 01:22:02 -0400

    Initial commit

--- PlagiarismDetector/src/np/plagiarism/DetectorEngine.java
+++ PlagiarismDetector/src/np/plagiarism/DetectorEngine.java
@@ -1,5 +1,7 @@
 package np.plagiarism;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nishanth on 10/14/2016.
@@ -8,14 +10,44 @@ public class DetectorEngine {
 
 
     public void run(PlagiarismDetector.Input in) {
-        // Get the synonyms
-        SynSet synonyms = new SynSet();
-        synonyms.store(in.synonym_file);
+        Parser fparser = new Parser();
+        List<Report> reports = new ArrayList<>();
 
-        // Create the hash of file 1 tuples
+        // Get the synonyms
+        obtainSynonyms(in);
+
+        // Create the hash of manuscripts tuples
+        parseManuscript(in, fparser);
 
         // Convert the second file tuples and hash them
 
-        // np.plagiarism.Report percentages
+        checkFudgings(in, fparser, reports);
+
+        // Render Report
+        renderReport(reports);
+    }
+
+    private void renderReport(List<Report> reports) {
+    }
+
+    private void checkFudgings(PlagiarismDetector.Input in, Parser fparser, List<Report> reports) {
+        int files_no = in.input_file.size();
+        Report rep;
+
+        for(int i = 0; i < files_no; i++) {
+            rep = new Report();
+            fparser.check(in.input_file.get(i), rep);
+            reports.add(rep);
+        }
+    }
+
+    private void parseManuscript(PlagiarismDetector.Input in, Parser fparser) {
+        Manuscript man = new Manuscript();
+        fparser.prepManu(in.manuscript, man);
+    }
+
+    private void obtainSynonyms(PlagiarismDetector.Input in) {
+        SynSet synonyms = new SynSet();
+        synonyms.store(in.synonym_file);
     }
 }
--- PlagiarismDetector/src/np/plagiarism/Parser.java
+++ PlagiarismDetector/src/np/plagiarism/Parser.java
@@ -4,4 +4,10 @@ package np.plagiarism;
  * Created by nishanth on 10/14/2016.
  */
 public class Parser {
+    public void prepManu(String manuscript, Manuscript man) {
+
+    }
+
+    public void check(String s, Report rep) {
+    }
 }
